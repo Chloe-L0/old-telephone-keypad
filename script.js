c4e8262ebca26929bcf1f695a762612836019b03
Chloe-O <chloeosbourne@yahoo.co.uk>
2024-11-22 19:32:48 +0000

    added number input field

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let numInput = document.getElementById("numInput");
 let keys = document.querySelectorAll("li span");
 let beep = document.createElement("audio");
 beep.src = "assets/beep.mp3";
 
+let charArray = [];
+
 const keyPress = () => {
   keys.forEach((key) => {
-    key.addEventListener("click", () => {
-      console.log(key.innerHTML);
+    key.addEventListener("mousedown", () => {
+      charArray.push(key.innerHTML);
+      numInput.innerHTML = charArray;
+      //   console.log(key.innerHTML);
       beep.play();
     });
   });
